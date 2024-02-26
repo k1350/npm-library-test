221d05acbc136130ea65ea0963f3e24d8a8e771b
Kimiko Mitamura <pom96egran46ate@gmail.com>
2024-02-26 20:38:38 +0900

    change directory

--- tsup.config.ts
+++ tsup.config.ts
@@ -6,12 +6,4 @@ export default defineConfig({
   format: ['cjs', 'esm'],
   clean: true,
   sourcemap: true,
-  // フォーマットごとに異なる出力ディレクトリを指定
-  esbuildOptions(options, { format }) {
-    if (format === 'esm') {
-      options.outdir = 'dist/esm';
-    } else if (format === 'cjs') {
-      options.outdir = 'dist/cjs';
-    }
-  },
 });
